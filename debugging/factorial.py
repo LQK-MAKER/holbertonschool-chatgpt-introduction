4dbddabefe2bdb977c746ef05ade00828315cb21
LQK-MAKER <lesmayoux.theo@gmail.com>
2025-01-13 10:32:41 +0100

    test

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,18 +5,17 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Décrémente n pour éviter une boucle infinie
+        n -= 1
     return result
 
-if len(sys.argv) > 1:
-    try:
-        input_value = int(sys.argv[1])
-        if input_value < 0:
-            print("Erreur : la factorielle n'est pas définie pour les nombres négatifs.")
-        else:
-            f = factorial(input_value)
-            print(f)
-    except ValueError:
-        print("Erreur : veuillez entrer un entier valide.")
-else:
-    print("Utilisation : ./factorial.py <nombre>")
+try:
+    n = int(sys.argv[1])
+    if n < 0:
+        print("Erreur : la factorielle n'est pas définie pour les nombres négatifs.")
+    else:
+        f = factorial(n)
+        print(f)
+except ValueError:
+    print("Erreur : veuillez entrer un nombre entier.")
+except IndexError:
+    print("Erreur : veuillez fournir un argument pour calculer la factorielle.")
